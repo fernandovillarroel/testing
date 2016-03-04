418573ea9e1a8035421b558a38ddf28606fbe934
Fernando Villarroel <ferv.sg@gmail.com>
2016-03-04 12:26:50 -0300

    Initializating the app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testingHtml5App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testingHtml5App
+ */
+angular.module('testingHtml5App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testingHtml5App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testingHtml5App
+ */
+angular.module('testingHtml5App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
